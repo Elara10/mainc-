6559920b474b99d7b2a8825f666362ce0ca14f26
IshaKumari10 <isha02740@gmail.com>
2023-12-28 18:05:20 +0530

    basic syntax done

---
+++ _2basicsyntax.cpp
@@ -0,0 +1,11 @@
+//Adding two numbers without taking an input
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=24;
+    int num2=34;
+    int result=num1+num2;;
+    cout<<result<<endl;
+    return 0;
+}
