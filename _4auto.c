4fb63aac16a58aad8c86be437b67836cdfbe3ddf
IshaKumari10 <isha02740@gmail.com>
2023-12-29 10:22:27 +0530

    auto

---
+++ _4auto.c
@@ -0,0 +1,13 @@
+//  C program to demonstrate 
+// auto keyword
+#include<stdio.h>
+// int printValue()
+// {
+//     auto int a=10;
+//     printf("%d",a);
+// }
+// int main()
+// {
+//     printValue();
+//     return 0;
+// }
