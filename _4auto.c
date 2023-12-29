aeb70f4e2c92730aa1afede94603139dd82aee7b
IshaKumari10 <isha02740@gmail.com>
2023-12-29 10:58:43 +0530

    remove int from auto

--- _4auto.c
+++ _4auto.c
@@ -1,13 +1,13 @@
 //  C program to demonstrate 
 // auto keyword
 #include<stdio.h>
-// int printValue()
-// {
-//     auto int a=10;
-//     printf("%d",a);
-// }
-// int main()
-// {
-//     printValue();
-//     return 0;
-// }
+int printValue()
+{
+    auto  a=10;
+    printf("%d",a);
+}
+int main()
+{
+    printValue();
+    return 0;
+}
