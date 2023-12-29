17c315b525b25c1ff31127168a8d3747fad25f03
IshaKumari10 <isha02740@gmail.com>
2023-12-29 12:23:15 +0530

    done with more eg of break and continue     12:23PM   12/29/23

--- _5breakcontinue.c
+++ _5breakcontinue.c
@@ -15,3 +15,55 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//break 29/12/23
+#include<stdio.h>
+int main()
+{
+    int i=0,j=0;
+    for(int  i=0;i<5;i++)
+    {
+        printf("i= %d ,j=",i);
+        for(int j=0;j<5;j++)
+        {
+            if(j==2)
+            break;
+            printf(" %d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+//contiue
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    int j=0;
+    for(int i=0;i<5;i++)
+    {
+        printf("i=%d,j=",i);
+        for(int j=0;j<5;j++)
+        {
+            if(j==2)
+            continue;
+           printf(" %d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
