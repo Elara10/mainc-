358ab88c30b737bc7d53772fccd0b295a75daca8
IshaKumari10 <isha02740@gmail.com>
2024-01-01 17:24:57 +0530

    double float

---
+++ _10doublefloat.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float f=0.3;
+    double d=10.67;
+    printf("Float value:%f\n",f);  //0.300000                                
+    printf("Double value:%f\n",d);//10.670000
+    return 0;
+}
