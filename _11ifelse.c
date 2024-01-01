4a8b78b5e968bb3a8772a47346d90666255e59e3
IshaKumari10 <isha02740@gmail.com>
2024-01-01 17:32:50 +0530

    if else

---
+++ _11ifelse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int  main()
+{
+    int a=90;
+    if(a<11)
+    {
+        printf("A is less than 11");
+
+    }
+    else{
+        printf("A is equal to or greater than 11");
+    }
+    return 0;
+}
