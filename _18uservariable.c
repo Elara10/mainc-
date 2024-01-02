af972a2138efcd94a84bd4b975e045a9f921b669
IshaKumari10 <isha02740@gmail.com>
2024-01-02 18:34:46 +0530

    use variable

---
+++ _18uservariable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int defined_var;
+    printf("Defined_var:%d\n",defined_var);
+     defined_var=2;
+    int init_var=10;
+    printf("Value  of defined_var after initialization: %d\n",defined_var);
+    printf("Value of init_var: %d",init_var);
+}
